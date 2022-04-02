552a6fc85c210583d0920edc3f639979424d9881
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-04-01 21:50:52 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int n;
+
+Console.WriteLine("Digite um numero:");
+n = Convert.ToInt32(Console.ReadLine());
+
+bool n2;
+
+n2 = (n < 0);
+
+if (n2)
+{
+    Console.WriteLine($"{n} é um numero negativo");
+}
+
+else{
+    Console.WriteLine();
+}
